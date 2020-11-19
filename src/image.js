87601a11eb00627d200591bf5c083e8c0e9d0b6b
kait9110 <71718232+kait9110@users.noreply.github.com>
2020-11-19 10:46:58 -0600

    image alt added and import image on temperature.js removed

--- src/image.js
+++ src/image.js
@@ -5,6 +5,7 @@ export default function Image() {
     <img
       src="https://openweathermap.org/img/wn/01d@2x.png"
       className="current-image"
+      alt="weather icon"
     />
   );
 }
--- src/temperature.js
+++ src/temperature.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "./image.js";
 import { Grid } from "@material-ui/core";
 
 export default function Temperature() {
